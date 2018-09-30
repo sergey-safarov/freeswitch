1b698b481dcc8d35330154bb5ba8602dbe9c83b3
Sergey Safarov <s.safarov@gmail.com>
2018-09-30 09:52:37 +0000

    Fixed text codec negotiation. Case video media goes before text media

--- src/switch_core_media.c
+++ src/switch_core_media.c
@@ -6066,7 +6066,7 @@ SWITCH_DECLARE(uint8_t) switch_core_media_negotiate_sdp(switch_core_session_t *s
 			}
 
 			if (!m->m_port) {
-				goto endsdp;
+				continue;
 			}
 
 			vmatch = 0;
@@ -6335,7 +6335,6 @@ SWITCH_DECLARE(uint8_t) switch_core_media_negotiate_sdp(switch_core_session_t *s
 		}
 	}
 
- endsdp:
 
 	if (rtcp_auto_audio || rtcp_auto_video) {
 		if (rtcp_auto_audio && !skip_rtcp && !got_audio_rtcp && audio_port) {
