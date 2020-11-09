35efed2ce9f89aa48b5fbba1590815bef569efaa
Sergey Safarov <s.safarov@gmail.com>
2020-11-09 21:09:24 +0000

    mod_conference: fixed crash

--- src/mod/applications/mod_conference/mod_conference.c
+++ src/mod/applications/mod_conference/mod_conference.c
@@ -1734,7 +1734,7 @@ switch_status_t conference_outcall_bg(conference_obj_t *conference,
 	if (var_event) {
 		call->var_event = *var_event;
 		var_event = NULL;
-	} else if (call->conference->outcall_export_member_id && (export_vars_member = conference_member_get(call->conference, call->conference->outcall_export_member_id))) {
+	} else if (call->conference && call->conference->outcall_export_member_id && (export_vars_member = conference_member_get(call->conference, call->conference->outcall_export_member_id))) {
 		switch_event_create(&call->var_event, SWITCH_EVENT_CHANNEL_DATA);
 		switch_channel_process_export(export_vars_member->channel, NULL, call->var_event, "conference_auto_outcall_export_vars");
 	}
