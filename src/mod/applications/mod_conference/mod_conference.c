175c073ab6a7f4dbc9312a5023a18a0fc0414c94
Sergey Safarov <s.safarov@gmail.com>
2020-11-23 11:41:45 +0000

    mod_conference: addedtion logs for participants managment

--- src/mod/applications/mod_conference/mod_conference.c
+++ src/mod/applications/mod_conference/mod_conference.c
@@ -2578,6 +2578,7 @@ SWITCH_STANDARD_APP(conference_function)
 	}
 
 	/* Remove the caller from the conference */
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "111 conference_member_del\n");
 	conference_member_del(member.conference, &member);
 
 	/* Put the original codec back */
